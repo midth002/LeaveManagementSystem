54bf690bdb180a8f6e8deec1e3214d418c052f8f
Andrew Midthun <midth002@gmail.com>
2024-11-09 09:57:47 -0600

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Andrew Midthun",
+                DateOfBirth = new DateTime(1997,01,23)
+            };
+            return View(data);
+        }
+    }
+}
